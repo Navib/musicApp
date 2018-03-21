3dd2c6fcfba8416659c2a553d70b8cd05ccd4bec
navib <ivan.baena.dev@gmail.com>
2018-03-21 19:18:44 -0400

    conveting duration

--- src/components/grids/top_results.js
+++ src/components/grids/top_results.js
@@ -9,22 +9,37 @@ import MoreVertIcon from "material-ui/svg-icons/navigation/more-vert";
 import IconMenu from "material-ui/IconMenu";
 import MenuItem from "material-ui/MenuItem";
 
+const convertToMin = duration => {
+  const dur = duration * 1000;
+  var minutes = Math.floor(dur / 60000);
+  var seconds = ((dur % 60000) / 1000).toFixed(0);
+
+  return <span>{`${minutes}:${seconds < 10 ? "0" : ""}${seconds}`}</span>;
+};
+
 const TopResults = props => {
   console.log("component: ", props);
+
   if (props.search.data === undefined) return <div>loading</div>;
 
   return (
     <div className="search-results top-results">
-      <List>
+      <div className="img-result">
+        <img src={props.search.data[0].artist.picture_big} />
+      </div>
+      <List className="top-result-list">
         <Subheader inset={true}>Top Results</Subheader>
-        {props.search.data.map(result => (
+        {props.search.data.slice(0, 5).map(result => (
           <ListItem
             key={result.id}
             leftAvatar={<Avatar src="images/ok-128.jpg" />}
             primaryText={result.title}
             secondaryText={
-              <p>
+              <p className="result-list-meta">
                 <span style={{ color: darkBlack }}>{result.artist.name}</span>
+                <span style={{ position: "absolute", top: 0, right: 0 }}>
+                  {convertToMin(result.duration)}
+                </span>
               </p>
             }
             secondaryTextLines={2}
